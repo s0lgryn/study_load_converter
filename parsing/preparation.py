8c35a0ee7d523f1597bb3ac4f01a19e900e27786
s0lgryn <filipp1411@gmail.com>
2023-05-28 02:58:59 +0300

    UI v1.0.1 Поменял пути к файлам из-за изменения структуры директорий

--- parsing/preparation.py
+++ parsing/preparation.py
@@ -13,8 +13,8 @@ def main():
 
 def check_is_dir_exists() -> None:
     """Проверяет, существует ли директория из переменной DATA_DIR, если нет то создает её"""
-    if not os.path.exists(DATA_DIR):
-        os.mkdir(DATA_DIR)
+    if not os.path.exists(f"../{DATA_DIR}"):
+        os.mkdir(f"../{DATA_DIR}")
 
 
 def check_is_file_exist() -> None:
@@ -24,7 +24,7 @@ def check_is_file_exist() -> None:
     """
     today_year = date.today().year
     next_year = date.today().year + 1
-    path_to_header = "requirements/header.xlsx"
+    path_to_header = "../requirements/header.xlsx"
     result_filename = f"{FILENAME}_{today_year}-{next_year}.xlsx"
     try:
         openpyxl.load_workbook(f"{DATA_DIR}/{result_filename}")
@@ -35,7 +35,7 @@ def check_is_file_exist() -> None:
         new_sheet = header.copy_worksheet(_sheet)  # Создаем его копию
         new_sheet.title = f"Тарификация {today_year}-{next_year}"  # Копии листа присваиваем новое имя
         header.remove(_sheet)  # Убираем лист со старым названием
-        save_workbook(header, f"{DATA_DIR}/{result_filename}")  # Сохраняем файл по новому адресу
+        save_workbook(header, f"../{DATA_DIR}/{result_filename}")  # Сохраняем файл по новому адресу
 
 
 if __name__ == '__main__':
