827df165a52687c5b91cd92592f8b05cbce924b0
s0lgryn <filipp1411@gmail.com>
2023-05-28 02:58:14 +0300

    UI v1.0.1 Добавил возможность указать год, валидацию ввода на это поле. Добавил очистку списка если выбирать файлы повторно.

--- gui/UI.py
+++ gui/UI.py
@@ -1,7 +1,11 @@
+import datetime
 import os.path
 import time
 import tkinter as tk
 from tkinter import ttk, filedialog
+
+from parsing import preparation
+from parsing.document_parser import check_filenames
 
 
 class Converter(tk.Tk):
@@ -15,38 +19,68 @@ class Converter(tk.Tk):
         self.files_list = []
         # Создаем переменную для хранения значения прогресса
         self.progress_value = tk.DoubleVar()
+
         # Создаем виджеты
         self.progress = ttk.Progressbar(self, length=350, mode='determinate', variable=self.progress_value)
-        self.listbox = tk.Listbox(self, )
-        self.label = tk.Label(self, text="Файлов выбрано: 0")
+
+        self.listbox = tk.Listbox(self)
+        self.listbox_counter = tk.Label(self, text="Файлов выбрано: 0")
+
+        self.year_label = tk.Label(self, text="Введите начало учебного года")
+        self.entry_var = tk.StringVar()
+        self.year_start = tk.Entry(self, textvariable=self.entry_var, validate="key")
+        self.year_start["validatecommand"] = (self.register(self.validate_input), '%P')
+
         self.select_button = tk.Button(self, text="Выбрать файлы", command=self.select_files, width=15)
         self.delete_button = tk.Button(self, text="Удалить", command=self.delete_file, width=15)
-        self.start_button = tk.Button(self, text="Начать", command=self.start_processing, width=15)
+        self.start_button = tk.Button(self, text="Начать", command=self.start_processing, width=15, state=tk.DISABLED)
 
         # Размещаем виджеты на экране
         self.progress.grid(row=0, column=0, padx=10, pady=10)
+
         self.listbox.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
-        self.label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-        self.select_button.grid(row=3, column=0, padx=10, pady=10, sticky="w")
-        self.delete_button.grid(row=3, column=0, padx=10, pady=10, sticky="n")
-        self.start_button.grid(row=3, column=0, padx=10, pady=10, sticky="e")
+        self.listbox_counter.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+
+        self.year_start.grid(row=3, column=0, padx=10, pady=10, sticky="e")
+        self.year_label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+
+        self.select_button.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+        self.delete_button.grid(row=4, column=0, padx=10, pady=10, sticky="n")
+        self.start_button.grid(row=4, column=0, padx=10, pady=10, sticky="e")
+
+    def toggle_start_button(self):
+        if self.listbox.size() > 0:
+            self.start_button.config(state=tk.NORMAL)
+        else:
+            self.start_button.config(state=tk.DISABLED)
+
+    def validate_input(self, value):
+        is_valid = value.isdigit()
+        if len(value) <= 4 and value >= str(datetime.date.today().year) and is_valid:
+            self.year_start.config(bg="#34eb5e")
+        else:
+            self.year_start.config(bg="#ff3333")
+        return is_valid
 
     def select_files(self):
-        files = filedialog.askopenfilenames()
         self.listbox.delete(0, tk.END)  # Очистка списка перед добавлением новых файлов
+        self.files_list.clear()
+        user_files = filedialog.askopenfilenames()
+        files = check_filenames(user_files)
         for file in files:
             self.files_list.append(file)
             name = os.path.basename(file)
             self.listbox.insert(tk.END, name)
-        self.label.config(text=f"Файлов выбрано: {self.listbox.size()}")
-        self.label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        self.listbox_counter.config(text=f"Файлов выбрано: {self.listbox.size()}")
+        self.listbox_counter.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        self.toggle_start_button()
 
     def delete_file(self):
         index = self.listbox.curselection()[0]
 
         del self.files_list[index]
         self.listbox.delete(index)
-        self.label.config(text=f"Файлов выбрано: {self.listbox.size()}")
+        self.listbox_counter.config(text=f"Файлов выбрано: {self.listbox.size()}")
 
     def update_progress(self):
         current_value = self.progress_value.get()
@@ -57,6 +91,9 @@ class Converter(tk.Tk):
         self.update_idletasks()
 
     def start_processing(self):
+        year_start = self.year_start.get()
+        preparation.main()
+        print(year_start)
         for file in self.files_list:
             time.sleep(1)
             self.update_progress()
