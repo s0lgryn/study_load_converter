a75cab26a5584e91efa7f5bea5d740ae22d88cc7
s0lgryn <filipp1411@gmail.com>
2023-05-31 16:38:09 +0300

    Поменял способ формирования названия файла. Теперь название формируется на основе данных из UI

--- parsing/preparation.py
+++ parsing/preparation.py
@@ -1,14 +1,13 @@
-import os
-from datetime import date
-
 import openpyxl
+import os
 from openpyxl.writer.excel import save_workbook
 from requirements.dependencies import DATA_DIR, FILENAME
 
 
-def main():
+def prepare(year_from_user):
     check_is_dir_exists()
-    check_is_file_exist()
+    check_is_file_exist(year_from_user)
+    get_filepath(year_from_user)
 
 
 def check_is_dir_exists() -> None:
@@ -17,13 +16,12 @@ def check_is_dir_exists() -> None:
         os.mkdir(f"../{DATA_DIR}")
 
 
-def check_is_file_exist() -> None:
-    # TODO написать нормальный коммент
+def check_is_file_exist(year_from_user) -> None:
     """Проверяет, существует ли в директории DATA_DIR, файл формата: FIlENAME_ТекущийГод_СледующийГод.
     Если файла нет, то он копирует шапку из requirements/header.xlsx и создает файл в папке converter
     """
-    today_year = date.today().year
-    next_year = date.today().year + 1
+    today_year = int(year_from_user)
+    next_year = int(year_from_user) + 1
     path_to_header = "../requirements/header.xlsx"
     result_filename = f"{FILENAME}_{today_year}-{next_year}.xlsx"
     try:
@@ -33,10 +31,18 @@ def check_is_file_exist() -> None:
         header = openpyxl.load_workbook(path_to_header)  # Загружаем лист с шапкой
         _sheet = header.active  # Выбираем лист
         new_sheet = header.copy_worksheet(_sheet)  # Создаем его копию
-        new_sheet.title = f"Тарификация {today_year}-{next_year}"  # Копии листа присваиваем новое имя
+        new_sheet.title = f"Тарификация"  # Копии листа присваиваем новое имя
+        new_sheet['A6'].value = f"Нагрузка СПО на {today_year}-{next_year} учебный год ОФО																			"
         header.remove(_sheet)  # Убираем лист со старым названием
         save_workbook(header, f"../{DATA_DIR}/{result_filename}")  # Сохраняем файл по новому адресу
 
 
+def get_filepath(year_from_user) -> str:
+    today_year = int(year_from_user)
+    next_year = int(year_from_user) + 1
+    result_filename = f"{FILENAME}_{today_year}-{next_year}.xlsx"
+    return f"../{DATA_DIR}/{result_filename}"
+
+
 if __name__ == '__main__':
-    main()
+    prepare(year_from_user=2023)
